07231792a9475220c2ee97a3d9007775f24ef97c
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-07-22 16:09:51 +0200

    changed the color of the plot

--- scatter_squares.py
+++ scatter_squares.py
@@ -6,7 +6,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use("seaborn-v0_8")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, color=(0, 0.8, 0), s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -15,7 +15,6 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set the range for each axis.
 ax.axis([0, 1100, 0, 1_100_000])
-ax.ticklabel_format(style='plain')
 
 # Set size of tick labels
 ax.tick_params(labelsize=14)
