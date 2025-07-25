5c5e93fe22bad54a80eae3d14e6d9ca0b5148276
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-07-25 15:54:37 +0200

    increased the amout of the steps and reduced size of the dots.

--- rw_visual.py
+++ rw_visual.py
@@ -5,7 +5,7 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
 # Make radnom walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
@@ -13,7 +13,7 @@ while True:
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-               edgecolors='none', s=15)
+               edgecolors='none', s=1)
     ax.set_aspect('equal')
 
     # Emphasize the first and last points.
