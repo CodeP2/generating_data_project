10dc2beca11c938a27e27e4a4b77ad294b6a7d4f
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-08-18 14:45:15 +0200

    showing all temperatures for the whole 2021 year for sitka

--- sitka_highs.py
+++ sitka_highs.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
-path = Path('weather_data/sitka_weather_07-2021_simple.csv')
+path = Path('weather_data/sitka_weather_2021_simple.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
 
 reader = csv.reader(lines)
@@ -28,7 +28,7 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red')
 
 # Format plot.
-ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_title("Daily High Temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperature (F)', fontsize=16)
