1063de0c8ae05ff43b07b0b615484b27830e45aa
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-08-21 14:04:42 +0200

    skips missing data

--- death_valley_high_lows.py
+++ death_valley_high_lows.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import csv
 from datetime import datetime
 
+import matplotlib.pyplot as plt
+
 
 path = Path('weather_data/death_valley_2021_simple.csv')
 lines = path.read_text(encoding="UTF-8").splitlines()
@@ -15,11 +17,15 @@ header_row = next(reader)
 dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
-    high = int(row[3])
-    low = int(row[4])
-    dates.append(current_date)
-    highs.append(high)
-    lows.append(low)
+    try:
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
 
 print(highs)
 
@@ -31,7 +37,8 @@ ax.plot(dates, lows, color='blue', alpha=0.5)
 ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
+title = "Daily High and Low Temperatures, 2021\nDeath Valley, CA"
+ax.set_title(title, fontsize=20)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperature (F)', fontsize=16)
