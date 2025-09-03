c82a94d8f2486de79929ef2d2c55c07c82f142f2
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-09-03 15:29:37 +0200

    Extracting location data and priting them out.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -16,9 +16,16 @@ all_eq_data = json.loads(contents)
 all_eq_dict= all_eq_data['features']
 # print(len(all_eq_dict))
 
-mags = []
+mags, lons, lats = [], [], []
 for eq_dict in all_eq_dict:
     mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
 
 print(mags[:10])
+print(lons[:5])
+print(lats[:5])
