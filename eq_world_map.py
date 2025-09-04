08d40cba6de3a785648a9936bbf6250305690514
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-09-04 13:20:15 +0200

    added colors for magnitudes and making the map look nicer.

--- eq_world_map.py
+++ eq_world_map.py
@@ -4,5 +4,11 @@ import plotly.express as px
 
 
 title = 'Global Earthquakes'
-fig = px.scatter_geo(lat=eq_explore_data.lats, lon=eq_explore_data.lons, size=eq_explore_data.mags, title=title)
+fig = px.scatter_geo(
+    lat=eq_explore_data.lats, lon=eq_explore_data.lons,
+    size=eq_explore_data.mags, title=title,
+    color=eq_explore_data.mags,
+    color_continuous_scale='Viridis',
+    labels={'color':'Magnitude'},
+    projection='natural earth')
 fig.show()
